b1671dedf89023f404523b76538888b64acfb138
David Healy <drtjprdave@gmail.com>
2022-09-04 18:46:31 -0600

    Starting to work on index.js.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const ProGen = require("inquirer");
+const inquirer = require("inquirer");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+const Engineer = require("./lib/engineer");
 
 
-
-// Double check to see if this needs to make the new user profiles.
-const proGen = ProGen();
-
-
-
+// // Double check to see if this needs to make the new user profiles.
+// const proGen = ProGen();
 
 const teamArray = [];
-
-
 
 inquirer
 .prompt([
@@ -21,35 +19,35 @@ inquirer
       },
       {
         type: "input",
-        message: "What is your id.?,
+        message: "What is your id.?",
         name: "id",
       },
       {
         type: "list",
         message: "what is your email?",
-        choices:"Engineer", "Manager"
-      
-      },
+        choices: "Engineer", "Manager",
+
+      }
 ])
 
-// Make a constructor function to take in employee data
-// name, age, ...
-function Employees(name, id, email) {
-  this.name = name;
-  this.id = id;
-  this.email = email;
-  return;
-}
+// // Make a constructor function to take in employee data
+// // name, age, ...
+// function Employees(name, id, email) {
+//   this.name = name;
+//   this.id = id;
+//   this.email = email;
+//   return;
+// }
 
-const Engineer = new Employees("David", 1, "drtjprdave@gmail.com");
-console.log(user1);
-const user2 = new Employees("David", 2, "drtjprdave@gmail.com");
-console.log(user1);
-const user3 = new Employees("David", 3, "drtjprdave@gmail.com");
-console.log(user1);
-const user4 = new Employees("David", 4, "drtjprdave@gmail.com");
-console.log(user1);
-const user5 = new Employees("David", 5, "drtjprdave@gmail.com");
-console.log(user1);
+// const Engineer = new Employees("David", 1, "drtjprdave@gmail.com");
+// console.log(user1);
+// const user2 = new Employees("David", 2, "drtjprdave@gmail.com");
+// console.log(user1);
+// const user3 = new Employees("David", 3, "drtjprdave@gmail.com");
+// console.log(user1);
+// const user4 = new Employees("David", 4, "drtjprdave@gmail.com");
+// console.log(user1);
+// const user5 = new Employees("David", 5, "drtjprdave@gmail.com");
+// console.log(user1);
 
-// TODO: create functions to getName, getId, getEmail getRole
+// // TODO: create functions to getName, getId, getEmail getRole
